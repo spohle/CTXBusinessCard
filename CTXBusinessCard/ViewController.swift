99f8367e88a1187f5f08289b770e42f31d97a9e1
Sven Pohle <svenpohle@tmcreavy-w10.activision.com>
2018-07-02 19:20:29 -0700

    Initial Commit

---
+++ CTXBusinessCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CTXBusinessCard
+//
+//  Created by Sven Pohle on 7/2/18.
+//  Copyright © 2018 Sven Pohle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
